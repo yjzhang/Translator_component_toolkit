11ff8922684c478bc8979d3eb1f434614ab6c6dd
yjzhang <yjzhang@cs.washington.edu>
2025-10-02 13:43:13 -0700

    data types

--- TCT/translator_node.py
+++ TCT/translator_node.py
@@ -2,6 +2,30 @@
 from dataclasses import dataclass
 
 @dataclass
+class TranslatorAttribute:
+    """
+    Class thar represents Translator node or edge attributes
+    """
+
+    attribute_type_id: str
+
+    value: object
+
+    value_type_id: str | None = None
+
+    original_attribute_name: str | None = None
+
+    value_url: str | None = None
+
+    attribute_source: str | None = None
+
+    description: str | None = None
+
+    attributes: list | None = None
+
+
+
+@dataclass
 class TranslatorNode:
     """
     Class for Translator graph nodes.
@@ -25,6 +49,9 @@ class TranslatorNode:
     curie_synonyms: list[str] | None = None
     "list of synonymous CURIE ids (in the same order as synonyms)"
 
+    attributes: list = []
+    "List of node attributes (which are key-value pairs."
+
     # identifier is just another way to access/set the CURIE.
     @property
     def identifier(self):
@@ -36,6 +63,12 @@ class TranslatorNode:
         """identifier is the CURIE id for the node."""
         self.curie = i
 
+    @property
+    def categories(self):
+        return self.types
+
+
+
 @dataclass
 class TranslatorEdge:
     """
@@ -46,7 +79,9 @@ class TranslatorEdge:
     "The subject is a CURIE id for a node."
 
     object: str
-    "The object is a CURIE id for a node."
+    "The obj (object) is a CURIE id for a node."
 
     predicate: str
     "Predicates"
+
+    attributes: list = []
