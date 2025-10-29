234c718f05c364c7aae132aa0a050e3c3d58baf4
yjzhang <yjzhang@cs.washington.edu>
2025-10-29 14:12:51 -0700

    add aragorn pathfinder url

--- TCT/ARAGORN_pathfinder.py
+++ TCT/ARAGORN_pathfinder.py
@@ -1,12 +1,19 @@
 from . import translator_query, node_normalizer
 
+# ARAGORN:
 
-def pathfinder(input_node1_id:str, input_node2_id:str):
+ARAGORN_URL = 'https://aragorn.test.transltr.io/aragorn/'
+
+def aragorn_pathfinder(input_node1_id:str, input_node2_id:str):
     """
+    ARAGORN docs: https://aragorn.test.transltr.io/aragorn/docs
+
     Params
     ------
     input_node1_id : str
         CURIE id for the first input node.
     input_node2_id : str
         CURIE id for the second input node.
+
     """
+    # TODO
